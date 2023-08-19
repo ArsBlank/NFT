0681d200a4366f6419a9e1eacad1f37fa6b20be5
ArsBlank <arsensheidd@gmail.com>
2023-08-19 11:45:03 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Background from "./components/Background.jsx";
 import Copyright from "./components/Copyright.jsx";
 import Form from "./components/Form.jsx";
-import logo from "./components/Logo.jsx";
+import Logo from "./components/Logo.jsx";
 
 function App() {
   return (
     <div className="sign-in">
       <div className="sign-in__info">
-        <logo />
-        <form />
-        <copyright />
+        <Logo />
+        <Form />
+        <Copyright />
       </div>
       <div className="sign-in__bg">
-        <background />
+        <Background />
       </div>
     </div>
   );
